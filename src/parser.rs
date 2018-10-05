61ddbc7a6f4adc51cb2a4bb6278bc8abb1f0e513
Nicole Mazzuca <npmazzuca@gmail.com>
2018-10-05 11:05:06 -0700

    continue working on parsing

--- src/parser.rs
+++ src/parser.rs
@@ -12,7 +12,7 @@ pub struct Parser<'i, 's> {
 #[derive(Debug)]
 pub struct Function<'i> {
   name: InternedString<'i>,
-  ret: InternedString<'i>,
+  ret_ty: InternedString<'i>,
   body: Expression<'i>,
 }
 
@@ -55,8 +55,39 @@ impl<'i, 's> Parser<'i, 's> {
     }
   }
 
-  pub fn parse_function(&mut self) -> Function<'i> {
-    panic!("not yet implemented")
+  fn get_ident(&mut self) -> InternedString<'i> {
+    match self.next_token() {
+      Token::Identifier(s) => s,
+      tok => panic!("Expected ident, found {:?}", tok),
+    }
+  }
+
+  fn eat_token(&mut self, tok: Token) {
+    let next_tok = self.next_token();
+    if tok != next_tok {
+      panic!("Expected {:?}, found {:?}", tok, next_tok)
+    }
+  }
+
+  fn parse_expression(&mut self) -> Expression<'i> {
+    match self.next_token() {
+      Token::IntegerLiteral(i) => Expression::IntegerLiteral(i),
+      Token::Identifier(s) => Expression::Name(s),
+      tok => panic!("Expected expression, found {:?}", tok),
+    }
+  }
+
+  fn parse_function(&mut self) -> Function<'i> {
+    let name = self.get_ident();
+    let () = self.eat_token(Token::OpenParen);
+    let () = self.eat_token(Token::CloseParen);
+    let () = self.eat_token(Token::Arrow);
+    let ret_ty = self.get_ident();
+    let () = self.eat_token(Token::OpenBrace);
+    let body = self.parse_expression();
+    let () = self.eat_token(Token::CloseBrace);
+
+    Function { name, ret_ty, body }
   }
 
   pub fn next_item(&mut self) -> Option<Item<'i>> {
--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -5,12 +5,14 @@ use unicode_xid::UnicodeXID;
 pub enum Token<'a> {
   Eof,
   KeywordFunc,
+  Operator(InternedString<'a>),
   Identifier(InternedString<'a>),
   IntegerLiteral(u64),
   OpenParen,
   CloseParen,
   OpenBrace,
   CloseBrace,
+  Arrow,
 }
 
 pub struct Lexer<'s> {
@@ -18,11 +20,22 @@ pub struct Lexer<'s> {
   iter: std::iter::Peekable<std::str::CharIndices<'s>>,
 }
 
-fn is_xid_start(ch: char) -> bool {
+fn is_operator_start(ch: char) -> bool {
+  const OPERATOR_START: &[char] = &['-'];
+
+  OPERATOR_START.contains(&ch)
+}
+fn is_operator_continue(ch: char) -> bool {
+  const OPERATOR_CONTINUE: &[char] = &['>'];
+
+  is_operator_start(ch) || OPERATOR_CONTINUE.contains(&ch)
+}
+
+fn is_ident_start(ch: char) -> bool {
   UnicodeXID::is_xid_start(ch)
 }
-fn is_xid_continue(ch: char) -> bool {
-  UnicodeXID::is_xid_continue(ch)
+fn is_ident_continue(ch: char) -> bool {
+  ch == '-' || ch == '\'' || UnicodeXID::is_xid_continue(ch)
 }
 fn base_of_letter(ch: char) -> Option<u32> {
   match ch {
@@ -42,16 +55,35 @@ impl<'s> Lexer<'s> {
     }
   }
 
-  fn match_keyword<'i>(
+  fn match_identifier<'i>(
     &self,
     intern: &'i Intern,
     first: usize,
     last: usize,
   ) -> Token<'i> {
-    let id = intern.add_string(&self.buffer[first..last]);
-    match id.as_str() {
+    let ident = &self.buffer[first..last];
+    match ident {
       "func" => Token::KeywordFunc,
-      _ => Token::Identifier(id),
+      _ => {
+        let id = intern.add_string(ident);
+        Token::Identifier(id)
+      }
+    }
+  }
+
+  fn match_operator<'i>(
+    &self,
+    intern: &'i Intern,
+    first: usize,
+    last: usize,
+  ) -> Token<'i> {
+    let ident = &self.buffer[first..last];
+    match ident {
+      "->" => Token::Arrow,
+      _ => {
+        let id = intern.add_string(ident);
+        Token::Operator(id)
+      }
     }
   }
 
@@ -63,17 +95,20 @@ impl<'s> Lexer<'s> {
     let buffer = &self.buffer;
     let len = buffer.len();
     let helper = move |last| {
-      Token::IntegerLiteral(
-        u64::from_str_radix(&buffer[first..last], base).unwrap(),
-      )
+      let buff = &buffer[first..last];
+      let int = match u64::from_str_radix(buff, base) {
+        Ok(i) => i,
+        Err(_) => panic!("ICE: invalid integer literal: {}", buff),
+      };
+      Token::IntegerLiteral(int)
     };
     loop {
       match self.iter.peek() {
         Some(&(idx, ch)) if ch.is_digit(base) => { self.iter.next(); },
-        Some(&(idx, ch)) if is_xid_continue(ch) => {
+        Some(&(idx, ch)) if is_ident_continue(ch) => {
           panic!("add whitespace after numbers, before identifier characters")
         }
-        Some(&(idx, _)) => return helper(idx),
+        Some(&(idx, _)) => return helper(idx - 1),
         None => return helper(self.buffer.len()),
       }
     }
@@ -92,18 +127,25 @@ impl<'s> Lexer<'s> {
           _ => return self.next_token(intern),
         };
       },
-      Some((start, ch)) if is_xid_start(ch) => loop {
+      Some((start, ch)) if is_ident_start(ch) => loop {
         match self.iter.peek() {
-          Some(&(idx, ch)) if is_xid_continue(ch) => self.iter.next(),
-          Some(&(idx, _)) => return self.match_keyword(intern, start, idx),
-          None => return self.match_keyword(intern, start, self.buffer.len()),
+          Some(&(idx, ch)) if is_ident_continue(ch) => self.iter.next(),
+          Some(&(idx, _)) => return self.match_identifier(intern, start, idx),
+          None => return self.match_identifier(intern, start, self.buffer.len()),
         };
       },
-      Some((_, '0')) => match self.iter.peek() {
-        Some(&(start, ch)) => {
+      Some((start, ch)) if is_operator_start(ch) => loop {
+        match self.iter.peek() {
+          Some(&(idx, ch)) if is_operator_continue(ch) => self.iter.next(),
+          Some(&(idx, _)) => return self.match_operator(intern, start, idx),
+          None => return self.match_operator(intern, start, self.buffer.len()),
+        };
+      },
+      Some((start0, ch0)) if ch0.is_digit(10) => match self.iter.peek() {
+        Some(&(_, ch1)) => {
           self.iter.next();
 
-          if let Some(base) = base_of_letter(ch) {
+          if let (true, Some(base)) = (ch0 == '0', base_of_letter(ch1)) {
             match self.iter.next() {
               Some((start, ch)) if ch.is_digit(base) => {
                 self.lex_number(start, base)
@@ -117,12 +159,10 @@ impl<'s> Lexer<'s> {
               }
               None => panic!("Unexpected EOF"),
             }
-          } else if ch.is_digit(10) {
-            self.lex_number(start, 10)
-          } else if is_xid_continue(ch) {
+          } else if is_ident_continue(ch1) {
             panic!("add whitespace after numbers, before identifier characters")
           } else {
-            Token::IntegerLiteral(0)
+            self.lex_number(start0, 10)
           }
         }
         _ => Token::IntegerLiteral(0),
